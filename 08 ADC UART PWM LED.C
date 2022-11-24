960ee4418b1c452ec43b65cb5302e590516404dd
Roboscience GTNK <79186183+GTNK002@users.noreply.github.com>
2022-11-24 06:04:14 +0530

    Create 08 ADC UART PWM LED.C

---
+++ 08 ADC UART PWM LED.C
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "driver/uart.h"
+#include "string.h"
+#include "driver/adc.h"
+#include "driver/ledc.h"
+
+static const int RX_BUF_SIZE = 1024;
+
+#define TXD_PIN (GPIO_NUM_17)
+#define RXD_PIN (GPIO_NUM_16)
+
+#define UART UART_NUM_2
+
+int num = 0;
+
+#define SAMPLE_CNT 32
+static const adc1_channel_t adc_channel = ADC_CHANNEL_4;
+#define LEDC_GPIO 27
+static ledc_channel_config_t ledc_channel;
+
+void init(void) 
+{
+    const uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_APB,
+    };
+
+    uart_driver_install(UART, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
+    uart_param_config(UART, &uart_config);
+    uart_set_pin(UART, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+
+    adc1_config_width(ADC_WIDTH_BIT_10);
+    adc1_config_channel_atten(adc_channel, ADC_ATTEN_DB_11);
+    ledc_timer_config_t ledc_timer = {
+        .duty_resolution = LEDC_TIMER_10_BIT,
+        .freq_hz = 10,
+        .speed_mode = LEDC_HIGH_SPEED_MODE,
+        .timer_num = LEDC_TIMER_0,
+        .clk_cfg = LEDC_AUTO_CLK,
+    };
+    ledc_timer_config(&ledc_timer);
+    ledc_channel.channel = LEDC_CHANNEL_0;
+    ledc_channel.duty = 0;
+    ledc_channel.gpio_num = LEDC_GPIO;
+    ledc_channel.speed_mode = LEDC_HIGH_SPEED_MODE;
+    ledc_channel.hpoint = 0;
+    ledc_channel.timer_sel = LEDC_TIMER_0;
+    ledc_channel_config(&ledc_channel);
+}
+
+static void tx_task(void *arg)
+{
+    while (1) {
+        uint32_t adc_val = 0;
+        adc_val += adc1_get_raw(adc_channel);
+        char cadc_val[5];
+        itoa(adc_val, cadc_val, 10);
+        uart_write_bytes(UART, cadc_val, 3);
+        printf("%d", adc_val);
+        printf("\n");
+        int led_speed = 730;
+        if(adc_val > 500) {
+            led_speed = 730;
+        }
+        else{ 
+            led_speed = 100;
+        }
+        ledc_set_duty(ledc_channel.speed_mode, ledc_channel.channel, led_speed);
+        ledc_update_duty(ledc_channel.speed_mode, ledc_channel.channel);
+        vTaskDelay(5000 / portTICK_RATE_MS);
+    }
+}
+
+void app_main(void)
+{
+    init();
+    xTaskCreate(tx_task, "uart_tx_task", 1024*2, NULL, configMAX_PRIORITIES-2, NULL);
+}
