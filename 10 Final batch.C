9db8563f742900637c1f9185a7ea0bdff2f50e2a
Roboscience GTNK <79186183+GTNK002@users.noreply.github.com>
2023-01-01 01:55:37 +0530

    Create 10 Final batch.C

---
+++ 10 Final batch.C
@@ -0,0 +1,124 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "driver/uart.h"
+#include "string.h"
+#include "driver/adc.h"
+#include "driver/ledc.h"
+#include "soc/timer_group_struct.h"
+#include "driver/timer.h"
+
+
+float checksec = 5.000;
+
+#define TIMER_INTR_SEL TIMER_INTR_LEVEL  
+#define TIMER_GROUP    TIMER_GROUP_0     
+#define TIMER_DIVIDER   80             
+#define TIMER_SCALE    (TIMER_BASE_CLK / TIMER_DIVIDER) 
+#define TIMER_FINE_ADJ   (0*(TIMER_BASE_CLK / TIMER_DIVIDER)/1000000)
+#define TIMER_INTERVAL0_SEC   (checksec)
+
+static const int RX_BUF_SIZE = 1024;
+#define TXD_PIN (GPIO_NUM_17)
+#define RXD_PIN (GPIO_NUM_16)
+#define UART UART_NUM_2
+
+#define SAMPLE_CNT 32
+static const adc1_channel_t adc_channel = ADC_CHANNEL_4;
+#define LEDC_GPIO 2
+static ledc_channel_config_t ledc_channel;
+
+void init(void) 
+{
+    const uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_APB,
+    };
+
+    uart_driver_install(UART, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
+    uart_param_config(UART, &uart_config);
+    uart_set_pin(UART, TXD_PIN, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+
+    adc1_config_width(ADC_WIDTH_BIT_10);
+    adc1_config_channel_atten(adc_channel, ADC_ATTEN_DB_11);
+    ledc_timer_config_t ledc_timer = {
+        .duty_resolution = LEDC_TIMER_10_BIT,
+        .freq_hz = 10,
+        .speed_mode = LEDC_HIGH_SPEED_MODE,
+        .timer_num = LEDC_TIMER_0,
+        .clk_cfg = LEDC_AUTO_CLK,
+    };
+    ledc_timer_config(&ledc_timer);
+    ledc_channel.channel = LEDC_CHANNEL_0;
+    ledc_channel.duty = 0;
+    ledc_channel.gpio_num = LEDC_GPIO;
+    ledc_channel.speed_mode = LEDC_HIGH_SPEED_MODE;
+    ledc_channel.hpoint = 0;
+    ledc_channel.timer_sel = LEDC_TIMER_0;
+    ledc_channel_config(&ledc_channel);
+}
+
+static void task001()
+{
+    uint32_t adc_val = 0;
+    adc_val += adc1_get_raw(adc_channel);
+    char cadc_val[5];
+    itoa(adc_val, cadc_val, 10);
+    uart_write_bytes(UART, cadc_val, 3);
+    int led_speed = 0;
+    if(adc_val > 500) {
+        led_speed = 730;
+    }
+    else{ 
+        led_speed = 100;
+    }
+    ledc_set_duty(ledc_channel.speed_mode, ledc_channel.channel, led_speed);
+    ledc_update_duty(ledc_channel.speed_mode, ledc_channel.channel);
+
+}
+
+void IRAM_ATTR timer_group0_isr(void *para){
+    int timer_idx = (int) para;
+     uint32_t intr_status = TIMERG0.int_st_timers.val;
+      if((intr_status & BIT(timer_idx)) && timer_idx == TIMER_0) {
+        TIMERG0.hw_timer[timer_idx].update = 1;
+        TIMERG0.int_clr_timers.t0 = 1;
+        TIMERG0.hw_timer[timer_idx].config.alarm_en = 1;
+        task001();
+      }
+}
+
+static void tg0_timer0_init()
+{
+    int timer_group = TIMER_GROUP_0;
+    int timer_idx = TIMER_0;
+    timer_config_t config;
+    config.alarm_en = 1;
+    config.auto_reload = 1;
+    config.counter_dir = TIMER_COUNT_UP;
+    config.divider = TIMER_DIVIDER;
+    config.intr_type = TIMER_INTR_SEL;
+    config.counter_en = TIMER_PAUSE;
+
+    timer_init(timer_group, timer_idx, &config);
+    timer_pause(timer_group, timer_idx);
+    timer_set_counter_value(timer_group, timer_idx, 0x00000000ULL);
+    timer_set_alarm_value(timer_group, timer_idx, (TIMER_INTERVAL0_SEC * TIMER_SCALE) - TIMER_FINE_ADJ);
+    timer_enable_intr(timer_group, timer_idx);
+    timer_isr_register(timer_group, timer_idx, timer_group0_isr, (void*) timer_idx, ESP_INTR_FLAG_IRAM, NULL);
+    timer_start(timer_group, timer_idx);
+}
+
+void app_main(void)
+{
+    init();
+    tg0_timer0_init();
+    task001();
+}
